c1ff9542bb99b6cc914791397333c8daf9e710ac
Aayush Dalvi <aayushdalvi100@gmail.com>
2021-05-23 17:11:09 +0530

    Update operations.py

--- sh-web-app/src/utilities/modules/sensor/operations.py
+++ sh-web-app/src/utilities/modules/sensor/operations.py
@@ -11,17 +11,10 @@ class SensorOps:
 
     def list_sensors(self):
         try:
-            # query = "SELECT * FROM {0};".format(Sensor.__tablename__)
-            # self.cur.execute(query)
-
             list(map(lambda each_sensor: self.senson_list.append({
                 "sensor_id":each_sensor.sensor_id
             }),self.session.query(Sensor).all()))
 
-            # get the all the sensors -->
-            # list(map(lambda each_sensor: self.senson_list.append({
-            #     "sensor_id":each_sensor['sensor_id']
-            # }),self.cur.fetchall()))
             return self.senson_list
         except Exception as e:
             print(str(e))
